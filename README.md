93ccde935d25b61d3d6c4f8759293ef9f353aa73
ukabuer <ukabuer@live.com>
2018-09-06 20:07:29 +0800

    polish

--- README.md
+++ README.md
@@ -1,29 +1,58 @@
-An unambitious static site generator, no magic.
+An unambitious static site generator, no magic inside.
 
-Define your routes, your data and connect the two by your templates.
+Define your routes and data, then connect the two with your templates.
 
-routes.json
-``` json
-[
-  {
+no implit rules,
 
-  },
-  {
-
-  }
-]
-```
-
-site.json
+`site.json`, define the global site data, accessing with variable `site`
 ``` json
 {
-  "title": "Marker"
+  "title": "this is the site title",
+  "routes": [
+    {
+      "path": "/",
+      "data": "contents/my_data.json",
+      "template": "templates/my_template.pug",
+      "deps": [
+        {
+          "path": "/:filename/",
+          "dir": "contents/blog",
+          "template": "templates/"
+        }
+      ]
+    },
+    {
+      "path": "/about/",
+      "data": "contents/about.json",
+      "template": "templates/my_template.pug",
+    }
+  ]
 }
 ```
 
-index.pug
+`my_data.json`, can be accessing with variable `data`
+```
+{
+  "title": "",
+  "slogan": "An unambitious static site generator, no magic inside"
+}
 ```
 
+`my_template.pug`,
+``` pug
+html
+  head
+    title= site.title
+  body
+    h1= data.title
+    h2= data.slogan
+    each dep in data.deps
+      h3
+        a(href=dep.path)= dep.data.title
 ```
 
 ## Features
+
+## Roadmap
+- [ ] pagination
+- [ ] routes syntax validation
--- bin/static.js
+++ bin/static.js
@@ -1,64 +1,74 @@
 #!/usr/bin/env node
 
-const debug = require('debug')('marker:entry');
-const path = require('path');
+const debug = require('debug')('static:entry');
 const program = require('commander');
-const createServer = require('../lib/server');
+const { readFile, ensureDir } = require('fs-extra');
+const createServer = require('../lib/serve');
 const parse = require('../lib/parse');
-const { write } = require('../lib/render');
-const { readFile } = require('../lib/utils');
+const generate = require('../lib/generate');
 
-const abs = name => path.resolve(__dirname, name);
-const config = {
-  site: abs('../test/contents/site.json'),
-  contentsDir: abs('../test/contents'),
-  templatesDir: abs('../test/templates'),
-  publicDir: abs('../test/public'),
-  port: 8080,
-};
-
-const getRoutes = () => readFile(config.site, 'utf8').then(async (json) => {
-  const data = JSON.parse(json);
-  const tasks = data.routes.map(route => parse(route, config));
-  const results = await Promise.all(tasks);
-  return results.reduce((all, cur) => all.concat(cur), []);
-});
+function getRoutes(config) {
+  return readFile(config.site, 'utf8')
+    .then(async (json) => {
+      const data = JSON.parse(json);
+      const tasks = data.routes.map(route => parse(route, config));
+      const results = await Promise.all(tasks);
+      return results.reduce((all, cur) => all.concat(cur), []);
+    });
+}
 
 program.version('0.1.0')
   .option('-s, --site <path>', 'Set site data file path, defaults to ./contents/site.json', './site.json')
-  .option('-c, --content <path>', 'Set the contents directory', './contents/')
-  .option('-t, --template <path>', 'Set the templates directory', './templates/');
+  .option('-c, --content <path>', 'Set the contents directory, defaults to ./contents', './contents/')
+  .option('-t, --template <path>', 'Set the templates directory', './templates/')
+  .option('-p, --public <path>', 'Set the public directory', './public');
 
 program
   .command('serve')
   .description('run a dev server at sepecific port, defaults to :8080')
   .option('-p, --port <port>', 'Set the server port', 8080)
   .action((options) => {
-    const { port } = options;
-    const { site, content, template } = options.parent;
-    getRoutes()
-    .then((routes) => {
-      debug(routes);
-      createServer(routes, config);
-    });
+    const {
+      site,
+      content,
+      template,
+      public: publicDir,
+    } = options.parent;
+    const config = {
+      port: options.port,
+      site,
+      content,
+      template,
+      public: publicDir,
+    };
+    ensureDir(publicDir)
+      .then(() => getRoutes(config))
+      .then((routes) => {
+        debug(routes);
+        createServer(routes, config);
+      });
   });
 
 program
   .command('gen')
   .description('generate HTML files in the public directory')
-  .option('-p, --public <path>', 'Set the public directory', './public')
   .action((options) => {
-    const { public } = options;
     const { site, content, template } = options.parent;
-    getRoutes()
-    .then((routes) => {
-      const tasks = routes.map(route => write(route, config));
-      return Promise.all(tasks);
-    })
-    .then(() => {
-      console.log('Generated!')
-      process.exit();
-    });
+    const config = {
+      public: options.parent.public,
+      site,
+      content,
+      template,
+    };
+    getRoutes(config)
+      .then((routes) => {
+        const tasks = routes.map(route => generate(route, config));
+        return Promise.all(tasks);
+      })
+      .then(() => {
+        console.log('Generated!');
+        process.exit();
+      });
   });
 
 program.parse(process.argv);
