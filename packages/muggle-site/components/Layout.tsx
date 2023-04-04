6e03e79323f33138941a620d4138f94ffb3826a4
ukabuer <ukabuer@live.com>
2023-04-04 21:32:40 +0800

    feat: responsive layout test

--- packages/muggle-site/components/Layout.tsx
+++ packages/muggle-site/components/Layout.tsx
@@ -9,6 +9,16 @@ const Layout: FunctionComponent = ({ children }) => {
     <main data-pjax-wrapper={true}>
       <Head>
         <Style>{css}</Style>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="theme-color" content="#ff7f7f" />
+        <meta name="msapplication-navbutton-color" content="#ff7f7f" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
       </Head>
       <Loading />
       <Header />
--- packages/muggle/src/index.ts
+++ packages/muggle/src/index.ts
@@ -2,3 +2,4 @@ export { default as Head } from "./components/Head.js";
 export { default as Style } from "./components/Style.js";
 export { default as hydrate, enablePJAX } from "./hydrate.js";
 export { useAppContext, AppContextData } from "./client.js";
+export { ServerRenderContextData } from "./context.js";
